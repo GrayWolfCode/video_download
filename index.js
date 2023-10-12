d746de497bb18960bc3c0945333026163a695fb1
GrayWolfCode <jlee888624@gmail.com>
2023-10-12 11:32:03 +0900

    strong

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/download', async (req, res) => {
         // Ensure headers are set for a download and match the response headers from the video source
         res.setHeader('Content-Type', response.headers['content-type']);
         res.setHeader('Content-Length', response.headers['content-length']);
-        res.setHeader('Content-Disposition', 'attachment; filename=map.mp4');
+        res.setHeader('Content-Disposition', 'attachment; filename=output_video.mp4');
         // Pipe the stream directly to the response
         response.data.pipe(res);
     } catch (error) {
